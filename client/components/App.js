5fea2164565e0439501e87167846ce7c3123df24
cf7 <cf7@users.noreply.github.com>
2021-08-07 19:15:27 -0700

    React component modules

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Panel } from './panel.js';
+
 export default function App() {
   return (
     <div>
       Hello World!
+      <Panel />
     </div>
   );
 }
---
+++ client/components/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Button extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button>
+        Search
+      </button>
+    );
+  }
+}
+
+export { Button };
