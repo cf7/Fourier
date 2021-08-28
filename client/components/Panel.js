6e5f56f21c11ed6e5218823c2a06e25adfa64538
cf7 <cf7@users.noreply.github.com>
2021-08-28 01:32:34 -0700

    adding bootstrap

--- client/components/Panel.js
+++ client/components/Panel.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { DropDown } from './Button.js';
+// import { DropDown } from './Button.js';
 import { TextArea } from './TextArea.js';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 class Panel extends React.Component {
 
@@ -13,7 +15,12 @@ class Panel extends React.Component {
     if (this.props.type == 'input') {
       return (
         <div className="panel">
-          <DropDown type='drop-down'/>
+          {/*<DropDown type='drop-down'/>*/}
+          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </DropdownButton>
           <TextArea className="user-input" handleContent={this.props.handleContent} />
         </div>
       );
