0d95ef4308d6e11c61b09b42895992449db83669
cf7 <cf7@users.noreply.github.com>
2021-11-05 03:47:14 -0700

    editor unit tests

--- client/components/__tests__/Editor.test.js
+++ client/components/__tests__/Editor.test.js
@@ -3,10 +3,10 @@
 */
 
 import React from 'react';
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
 import Editor from '../Editor.js';
 import { configure, shallow, mount, render } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
@@ -16,26 +16,27 @@ describe("Editor", () => {
     mount(<Editor />);
   });
 
-  describe("Editor Controls", () => {
+  describe("Controls", () => {
     it("show font size menu on Font Size button click", () => {
       const wrapper = mount(<Editor />);
       wrapper.find('button.dropdown-toggle').simulate('click');
       expect(wrapper.exists('div.dropdown-menu.show')).toBe(true);
       wrapper.unmount();
     });
+  });
 
+  describe("TextArea", () => {
     it("changes input when typed in", () => {
       const onChangeMock = jest.fn();
-      const wrapper = shallow(<Editor onChange={onChangeMock} />);
-      console.log(wrapper.simulate);
-      wrapper.simulate('onChange');
+      const wrapper = mount(<Editor onChange={onChangeMock} />);
+      wrapper.find('textarea').simulate('change');
       expect(onChangeMock.mock.calls.length).toBe(1);
       wrapper.unmount();
     });
 
     it("adjusts font size for displayed text", () => {
       const editorFontSizes = [17, 18, 19, 20];
-      const wrapper = shallow(<Editor editorFontSize={17} />);
+      const wrapper = mount(<Editor editorFontSize={17} />);
       editorFontSizes.forEach((fs) => {
         wrapper.setProps({ editorFontSize: fs });
         expect(wrapper.prop("editorFontSize")).toBe(fs);
